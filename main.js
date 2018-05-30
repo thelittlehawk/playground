b888c379e52245e1950f7e5698818eaf645f449b
faik <faik.catibusic@gmail.com>
2018-05-30 14:59:19 +0200

    init

---
+++ main.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+const products = [
+  { "name": "Test product", "price": 100 }
+];
+
+app.get('/ping', function(req, res) {
+  res.send({ "status": "pong" })
+})
+
+app.get('/products', function(req, res) {
+  res.send(products)
+})
+
+app.post('/products', function(req, res) {
+  products.push({
+    "name": req.body.name,
+    "price": req.body.price
+  })
+  res.send({"status": "okay"})
+})
+
+app.listen(8001)
