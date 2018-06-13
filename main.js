56feafbc58b32eb80e2a4e85cafb1832abd60456
faik <faik.catibusic@gmail.com>
2018-06-13 17:21:16 +0200

    Login.

--- main.js
+++ main.js
@@ -12,8 +12,25 @@ app.get('/ping', function(req, res) {
   res.send({ "status": "pong" })
 })
 
+app.post('/login', function(req, res) {
+  if (req.params.email === "test@ssst.ba" && req.params.pass === "pass") {
+    res.send({"status": "valid"})
+  } else {
+    res.send({"status": "invalid"})
+  }
+})
+
 app.get('/products', function(req, res) {
   res.send(products)
+})
+
+app.get('/products/:id', function(req, res) {
+  res.send(products[req.params.id])
+})
+
+app.delete('/products/:id', function(req, res) {
+  products.splice(req.params.id, 1)
+  res.send({"status": "deleted"})
 })
 
 app.post('/products', function(req, res) {
@@ -21,7 +38,7 @@ app.post('/products', function(req, res) {
     "name": req.body.name,
     "price": req.body.price
   })
-  res.send({"status": "okay"})
+  res.send({"status": "created"})
 })
 
 app.listen(process.env.PORT || 8001)
